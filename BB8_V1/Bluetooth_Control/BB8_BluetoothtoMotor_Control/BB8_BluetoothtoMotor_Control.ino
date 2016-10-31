0bdf7683e3b0bc39902afb84953ad13774b6117d
JLReitz <JLReitz@live.com>
2016-10-31 12:42:16 -0700

    Just changed some stuff. The motors should be able to do more than just go forward and backwards now... fingers crossed and knock on wood. I also added some fun comments. I'm quite proud of them

--- BB8_V1/Bluetooth_Control/BB8_BluetoothtoMotor_Control/BB8_BluetoothtoMotor_Control.ino
+++ BB8_V1/Bluetooth_Control/BB8_BluetoothtoMotor_Control/BB8_BluetoothtoMotor_Control.ino
@@ -7,7 +7,7 @@
  *  
  * Written by Jensen Reitz and Marcus Blaisdell of WSU Robotics
  *  
- *  - Utilizes the HC-05 Bluetooth Module to transmit the signal and thr HC-06 Bluetooth Module
+ *  - Utilizes the HC-05 Bluetooth Module to transmit the signal and the HC-06 Bluetooth Module
  *    to recieve the signal
  */
 
@@ -15,20 +15,19 @@
 
 #define DEBUG
 
-//Constant Variables -----------------------------------------------------------------
+//Constant Variables ----------------------------------------------------------------------------------
 const int joystick_xPin = 4, joystick_yPin = 5; //Joystick pins
-const int a_dir1 = 10, a_dir2 = 9, a_control = 6, b_dir1 = 8, b_dir2 = 7, b_control = 5; //Motor pins
+const int r_dir1 = 10, r_dir2 = 9, r_control = 6, l_dir1 = 8, l_dir2 = 7, l_control = 5; //Motor pins
+const int neutral_x = 504, neutral_y = 490; //Neutral values for x_joystick and y_joystick
 
-// Global Variables ------------------------------------------------------------------
-int joystick_x = 0, joystick_y = 0; //Values to be read in from the joystick (x is neutral at 504, y is neutral at 490)
-char c = '\n'; //For reading from SoftwareSerial
-String bt_str = "\0";
+// Global Variables -----------------------------------------------------------------------------------
+int joystick_x = 0, joystick_y = 0; //Values to be read in from the joystick
 
-// Objects ---------------------------------------------------------------------------
+// Objects --------------------------------------------------------------------------------------------
 SoftwareSerial bt_serial(2, 3); // RX | TX
 
 
-// Start Program *********************************************************************
+// Start Program ****************************************************************************************************************************************************************
 
 
 void setup() 
@@ -52,9 +51,16 @@ void loop()
 }
 
 
-// Public Functions ********************************************************************
+// Public Functions *************************************************************************************************************************************************************
 
 
+//Assigns the motors their specific speed values and directional current based on the speed values ------------------------------------------------------------------------------
+//  assigned via readSerial()
+//- Can vary between full forward, turning while moving forward, turning while stopped, turning while
+//  moving backwards, full backwards, and full stop
+//- Implements analogWrite() due to the motors inability to recieve a high enough voltage when treated
+//  as servos within the program... Please don't hate me for making it more complicated than it needs
+//  to be, I promise I tried to make it simple
 void motorControl()
 {
   #ifdef DEBUG
@@ -66,35 +72,87 @@ void motorControl()
     Serial.println(signalFilter(joystick_y, 1));
   #endif
 
+  int x_signal = signalFilter(joystick_x, 0);
   int y_signal = signalFilter(joystick_y, 1);
   
-  if (y_signal>490)
-  {    
-    digitalWrite(a_dir1, HIGH);
-    digitalWrite(a_dir2, LOW);
-    analogWrite(a_control, joystick_y*.2490234375);
-    
-    digitalWrite(b_dir1, HIGH);
-    digitalWrite(b_dir2, LOW);
-    analogWrite(b_control, joystick_y*.2490234375);
+  if (y_signal > 0) //The droid is supposed to go forward
+  {  
+    digitalWrite(r_dir1, HIGH);
+    digitalWrite(r_dir2, LOW);
+    digitalWrite(l_dir1, HIGH);
+    digitalWrite(l_dir2, LOW);
+     
+    if(x_signal > 0) //Turn right
+    {
+      analogWrite(r_control, y_signal-x_signal);
+      analogWrite(l_control, y_signal);
+    }
+    else if(x_signal < 0) //Turn left
+    {
+      analogWrite(r_control, y_signal);
+      analogWrite(l_control, y_signal-x_signal);
+    }
+    else //Go completely straight
+    {
+      analogWrite(r_control, y_signal);
+      analogWrite(l_control, y_signal);
+    }
   }
-  else if (y_signal<490)
+  else if (y_signal < 0) //The droid is supposed to go backwards
   { 
-    digitalWrite(a_dir1, LOW);
-    digitalWrite(a_dir2, HIGH);
-    analogWrite(a_control, (1024-joystick_y)*.2490234375);
-    
-    digitalWrite(b_dir1, LOW);
-    digitalWrite(b_dir2, HIGH);
-    analogWrite(b_control, (1024-joystick_y)*.2490234375);
+    digitalWrite(r_dir1, LOW);
+    digitalWrite(r_dir2, HIGH);
+    digitalWrite(l_dir1, LOW);
+    digitalWrite(l_dir2, HIGH);
+     
+    if(x_signal > 0) //Turn right
+    {
+      analogWrite(r_control, y_signal-x_signal);
+      analogWrite(l_control, y_signal);
+    }
+    else if(x_signal < 0) //Turn left
+    {
+      analogWrite(r_control, y_signal);
+      analogWrite(l_control, y_signal-x_signal);
+    }
+    else //Go completely straight
+    {
+      analogWrite(r_control, y_signal);
+      analogWrite(l_control, y_signal);
+    }
   }
-  else
+  else if(!y_signal && x_signal) //The robot is only supposed to turn
   {
-    analogWrite(a_control, 0);
-    analogWrite(b_control, 0);
+    if(x_signal>0) //Turn right
+    {
+      digitalWrite(r_dir1, LOW);
+      digitalWrite(r_dir2, HIGH);
+      analogWrite(r_control, x_signal);
+      
+      digitalWrite(l_dir1, HIGH);
+      digitalWrite(l_dir2, LOW);
+      analogWrite(l_control, x_signal);
+    }
+    else //Turn left
+    {
+      digitalWrite(r_dir1, HIGH);
+      digitalWrite(r_dir2, LOW);
+      analogWrite(r_control, -x_signal);
+      
+      digitalWrite(l_dir1, LOW);
+      digitalWrite(l_dir2, HIGH);
+      analogWrite(l_control, -x_signal);
+    }
+  }
+  else //The droid is supposed to come to a full stop
+  {
+    analogWrite(r_control, 0);
+    analogWrite(l_control, 0);
   }
 }
 
+//Reads the bt_serial input and parses the included integers into their respective joystick values ------------------------------------------------------------------------------
+//- That is all... You expected something more?
 void readSerial()
 { 
   int i = 0;
@@ -110,12 +168,12 @@ void readSerial()
     joystick_x = bt_serial.parseInt();
     joystick_y = bt_serial.parseInt();
     
-    if (bt_serial.read()=='\n')
+    if (bt_serial.read() == '\n')
       return; //End if newline
     
   } //End while loop
   
-  if (bt_serial.available()<0)
+  if (bt_serial.available() < 0)
   {
     bt_serial.end();
     bt_serial.begin(9600);
@@ -123,18 +181,29 @@ void readSerial()
 
 } //End readSerial function
 
+//Filters the joystick value based on it's neutral value an the value that is read in (value that means zero movement in that axis) ---------------------------------------------
+//- The joystick is not super precise, so everytime you return it to the center position, it's value may be a bit different
+//- This function responds to that uncertainty by comparing the actual value to that axis' "neutral value"
+//- If the value is within 10 of the axis' neutral state, then the function will return that neutral value instead of the actual one,
+//  Because it is presumed that the user meant for there to be no movement
+//- This function also converts the analogRead() values that were sent over bluetooth to the smaller analogWrite() values to reduce
+//  redundancy in the program later on (converts 1024 to 255)
 int signalFilter(int joystickValue, int filterCase)
 {
   switch(filterCase)
   {
     case 0: //Joystick x
-      if(abs(504-joystickValue)>10)
-        return joystickValue;
+      if(abs(neutral_x-joystickValue) > 10)
+        return (joystickValue > neutral_x) ? (joystickValue * .2490234375) : (-(1024-joystickValue) * .2490234375);
+      else
+        return 0;
         
       break;
     case 1: //Joystick y
-      if(abs(490-joystickValue)>10)
-        return joystickValue;
+      if(abs(neutral_y-joystickValue) > 10)
+        return (joystickValue > neutral_y) ? (joystickValue * .2490234375) : (-(1024-joystickValue) * .2490234375);
+      else
+        return 504;
 
       break;
   }
--- BB8_V1/Bluetooth_Control/BB8_JoysticktoBluetooth_Control/BB8_JoysticktoBluetooth_Control.ino
+++ BB8_V1/Bluetooth_Control/BB8_JoysticktoBluetooth_Control/BB8_JoysticktoBluetooth_Control.ino
@@ -15,18 +15,18 @@
 
 #define DEBUG
 
-//Constant Variables -----------------------------------------------------------------
+//Constant Variables -------------------------------------------------------------------------------------------------------------------------------------------
 const int joystick_xPin = 3, joystick_yPin = 4;
 
-// Global Variables ------------------------------------------------------------------
+// Global Variables --------------------------------------------------------------------------------------------------------------------------------------------
 int joystick_x = 0, joystick_y = 0, past_joystick_x = 0, past_joystick_y = 0; //Values to be read in from the joystick (x is neutral at 504, y is neutral at 490)
 String bt_str = "\0";
 
-// Objects ---------------------------------------------------------------------------
+// Objects -----------------------------------------------------------------------------------------------------------------------------------------------------
 SoftwareSerial bt_serial(2, 3); // RX | TX
 
 
-// Start Program *********************************************************************
+// Start Program ****************************************************************************************************************************************************************
 
 
 void setup() 
@@ -71,12 +71,20 @@ void loop()
 }
 
 
-// Public Functions ********************************************************************
+// Public Functions *************************************************************************************************************************************************************
 
 
+//Filters the joystick value based on it's neutral value an the value that is read in (value that means zero movement in that axis) ---------------------------------------------
+//- The joystick is not super precise, so even when you're holding it in place, the value will vary slightly. This can cause some
+//  unwanted movement in the motors where they should normally be pretty steady (since you're holding the joystick in place,
+//  regardless of position, duhhhh)
+//- This function responds to that annoyance by comparing the new (changed) joystick value to the old value and returning false
+//  if they are too similar, thus stating that the joystick is actually being held in place and the joystick values in this program
+//  should not change
+//- Once the values do change enough to constitute the movement being intentional by the user, THEN the values will change accordingly
 bool signalFilter(int newValue, int oldValue)
 {
-  if(abs(newValue-oldValue)>10)
+  if(abs(newValue-oldValue) > 10)
     return true;
   else
     return false;
