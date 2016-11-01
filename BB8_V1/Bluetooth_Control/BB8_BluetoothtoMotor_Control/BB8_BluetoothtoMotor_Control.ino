1f0108b22033d862aaeae414716449014b4fd4c1
JLReitz <JLReitz@live.com>
2016-10-31 23:20:50 -0700

    Changed the motorControl() code so that it would ACTUALLY work now. The BB8 can drive itself in all directions as of 5 minutes ago. I also added some stuff to start making the head move

--- BB8_V1/Bluetooth_Control/BB8_BluetoothtoMotor_Control/BB8_BluetoothtoMotor_Control.ino
+++ BB8_V1/Bluetooth_Control/BB8_BluetoothtoMotor_Control/BB8_BluetoothtoMotor_Control.ino
@@ -12,19 +12,24 @@
  */
 
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
-#define DEBUG
+//#define DEBUG
+//#define DEBUG_MOTORCONTROL
+#define DEBUG_HEAD
 
 //Constant Variables ----------------------------------------------------------------------------------
 const int joystick_xPin = 4, joystick_yPin = 5; //Joystick pins
 const int r_dir1 = 10, r_dir2 = 9, r_control = 6, l_dir1 = 8, l_dir2 = 7, l_control = 5; //Motor pins
+const int servoPin = 11; //Servo pin
 const int neutral_x = 504, neutral_y = 490; //Neutral values for x_joystick and y_joystick
 
 // Global Variables -----------------------------------------------------------------------------------
-int joystick_x = 0, joystick_y = 0; //Values to be read in from the joystick
-
+int joystick_x = 0, joystick_y = 0, joystick_btn = 0; //Values to be read in from the joystick
+char headDir = 'r'; //Changes between 'r' and 'l' for right and left
 // Objects --------------------------------------------------------------------------------------------
 SoftwareSerial bt_serial(2, 3); // RX | TX
+Servo head;
 
 
 // Start Program ****************************************************************************************************************************************************************
@@ -39,6 +44,8 @@ void setup()
     // start communication with the HC-05 using 9600
     bt_serial.begin(9600);
     Serial.println("bt_serial started at 9600");
+
+    head.attach(servoPin);
 }
 
 void loop()
@@ -48,6 +55,18 @@ void loop()
   
   //Assign joystick values to the motor signals
   motorControl();
+
+  
+  //Rotate the head if the joystick button is pressed
+  if(joystick_btn)
+    rotateHead();
+
+  #ifdef DEBUG_HEAD
+    Serial.print("Head Turn: ");
+    Serial.print(joystick_btn);
+    Serial.print("  Head Servo: ");
+    Serial.println(head.read());
+  #endif
 }
 
 
@@ -68,12 +87,17 @@ void motorControl()
     Serial.print(joystick_x);
     Serial.print("  joystick_y: ");
     Serial.println(joystick_y);
-    Serial.print("signalFilter: ");
-    Serial.println(signalFilter(joystick_y, 1));
   #endif
 
   int x_signal = signalFilter(joystick_x, 0);
   int y_signal = signalFilter(joystick_y, 1);
+  
+  #ifdef DEBUG_MOTORCONTROL
+    Serial.print("x_signal: ");
+    Serial.print(x_signal);
+    Serial.print("  y_signal: ");
+    Serial.println(y_signal);
+  #endif
   
   if (y_signal > 0) //The droid is supposed to go forward
   {  
@@ -84,13 +108,13 @@ void motorControl()
      
     if(x_signal > 0) //Turn right
     {
-      analogWrite(r_control, y_signal-x_signal);
-      analogWrite(l_control, y_signal);
+      analogWrite(r_control, 255-x_signal);
+      analogWrite(l_control, 255);
     }
     else if(x_signal < 0) //Turn left
     {
-      analogWrite(r_control, y_signal);
-      analogWrite(l_control, y_signal-x_signal);
+      analogWrite(r_control, 255);
+      analogWrite(l_control, 255-abs(x_signal));
     }
     else //Go completely straight
     {
@@ -107,18 +131,18 @@ void motorControl()
      
     if(x_signal > 0) //Turn right
     {
-      analogWrite(r_control, y_signal-x_signal);
-      analogWrite(l_control, y_signal);
+      analogWrite(r_control, 255-x_signal);
+      analogWrite(l_control, 255);
     }
     else if(x_signal < 0) //Turn left
     {
-      analogWrite(r_control, y_signal);
-      analogWrite(l_control, y_signal-x_signal);
+      analogWrite(r_control, 255);
+      analogWrite(l_control, 255-abs(x_signal));
     }
     else //Go completely straight
     {
-      analogWrite(r_control, y_signal);
-      analogWrite(l_control, y_signal);
+      analogWrite(r_control, abs(y_signal));
+      analogWrite(l_control, abs(y_signal));
     }
   }
   else if(!y_signal && x_signal) //The robot is only supposed to turn
@@ -137,17 +161,46 @@ void motorControl()
     {
       digitalWrite(r_dir1, HIGH);
       digitalWrite(r_dir2, LOW);
-      analogWrite(r_control, -x_signal);
+      analogWrite(r_control, abs(x_signal));
       
       digitalWrite(l_dir1, LOW);
       digitalWrite(l_dir2, HIGH);
-      analogWrite(l_control, -x_signal);
+      analogWrite(l_control, abs(x_signal));
     }
   }
   else //The droid is supposed to come to a full stop
   {
     analogWrite(r_control, 0);
     analogWrite(l_control, 0);
+  }
+}
+
+void rotateHead()
+{
+  int headPos = head.read();
+  
+  if(headDir == 'r')
+  {
+    if(headPos<180)
+    {
+      head.write(headPos+1);
+      delay(10);
+    }
+      
+
+    if(headPos == 180)
+      headDir = 'l';
+  }
+  else if(headDir == 'l')
+  {
+    if(headPos>0)
+    {
+      head.write(headPos-1);
+      delay(10);
+    }
+
+    if(headPos == 0)
+      headDir = 'r';
   }
 }
 
@@ -167,6 +220,7 @@ void readSerial()
     
     joystick_x = bt_serial.parseInt();
     joystick_y = bt_serial.parseInt();
+    joystick_btn = bt_serial.parseInt();
     
     if (bt_serial.read() == '\n')
       return; //End if newline
@@ -193,19 +247,20 @@ int signalFilter(int joystickValue, int filterCase)
   switch(filterCase)
   {
     case 0: //Joystick x
-      if(abs(neutral_x-joystickValue) > 10)
+      if(abs(neutral_x-joystickValue) > 20)
         return (joystickValue > neutral_x) ? (joystickValue * .2490234375) : (-(1024-joystickValue) * .2490234375);
       else
         return 0;
         
       break;
     case 1: //Joystick y
-      if(abs(neutral_y-joystickValue) > 10)
+      if(abs(neutral_y-joystickValue) > 20)
         return (joystickValue > neutral_y) ? (joystickValue * .2490234375) : (-(1024-joystickValue) * .2490234375);
       else
-        return 504;
+        return 0;
 
       break;
   }
 }
+
 
--- BB8_V1/Bluetooth_Control/BB8_JoysticktoBluetooth_Control/BB8_JoysticktoBluetooth_Control.ino
+++ BB8_V1/Bluetooth_Control/BB8_JoysticktoBluetooth_Control/BB8_JoysticktoBluetooth_Control.ino
@@ -16,10 +16,10 @@
 #define DEBUG
 
 //Constant Variables -------------------------------------------------------------------------------------------------------------------------------------------
-const int joystick_xPin = 3, joystick_yPin = 4;
+const int joystick_xPin = 3, joystick_yPin = 4, joystick_btnPin = 4;
 
 // Global Variables --------------------------------------------------------------------------------------------------------------------------------------------
-int joystick_x = 0, joystick_y = 0, past_joystick_x = 0, past_joystick_y = 0; //Values to be read in from the joystick (x is neutral at 504, y is neutral at 490)
+int joystick_x = 0, joystick_y = 0, joystick_btn = 0, past_joystick_x = 0, past_joystick_y = 0; //Values to be read in from the joystick (x is neutral at 504, y is neutral at 490)
 String bt_str = "\0";
 
 // Objects -----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -38,6 +38,8 @@ void setup()
     // start communication with the HC-06 using 9600
     bt_serial.begin(9600);
     Serial.println("bt_serial started at 9600");
+
+    pinMode(4, INPUT);
 }
  
 void loop()
@@ -45,6 +47,7 @@ void loop()
     //Read in values from the joystick
     joystick_x = analogRead(joystick_xPin);
     joystick_y = analogRead(joystick_yPin);
+    joystick_btn = digitalRead(joystick_btnPin);
 
     if(signalFilter(joystick_x ,past_joystick_x) || signalFilter(joystick_y, past_joystick_y))
     {
@@ -55,11 +58,27 @@ void loop()
       bt_str.concat(joystick_x);
       bt_str.concat(',');
       bt_str.concat(joystick_y);
+      bt_str.concat(',');
+      bt_str.concat(joystick_btn);
       bt_str.concat('x');
       bt_str.concat('\n');
   
       past_joystick_x = joystick_x;
       past_joystick_y = joystick_y;
+    }
+    else
+    {
+      //Clear the string to rewrite it
+      bt_str = "\0";
+      
+      //Parse values into string
+      bt_str.concat(past_joystick_x);
+      bt_str.concat(',');
+      bt_str.concat(past_joystick_y);
+      bt_str.concat(',');
+      bt_str.concat(joystick_btn);
+      bt_str.concat('x');
+      bt_str.concat('\n');
     }
 
       //Upload the string into bt_serial
